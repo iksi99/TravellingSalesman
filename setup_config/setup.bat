bdea9343c7f7e8756f5d9bde43d595f9005da0e3
Илија Анастасијевић <ai180108d@student.etf.bg.ac.rs>
2020-06-03 18:03:34 +0200

    Shortcut working directory fix. Final feature complete build.

--- setup_config/setup.bat
+++ setup_config/setup.bat
@@ -1,7 +1,8 @@
-for /f "tokens=3" %%a in ('REG QUERY HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\VisualStudio\14.0\VC\Runtimes\x64 /v Bld ^|findstr /ri "REG_DWORD"') do set VCBLD=%%a
+REG QUERY HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\VisualStudio\14.0\VC\Runtimes\x64
 
-if %VCBLD% LSS 0x7030 (vcredist_x64.exe)
+if %ERRORLEVEL% NEQ 0 vcredist_x64.exe else GOTO CHECK_VERSION
 
+:INSTALL
 rmdir /s /q "C:\TravellingSalesmanSolver"
 
 mkdir "C:\TravellingSalesmanSolver"
@@ -21,8 +22,18 @@ echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT%
 echo sLinkFile = "%USERPROFILE%\Desktop\Travelling Salesman Solver.lnk" >> %SCRIPT%
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT%
 echo oLink.TargetPath = "C:\TravellingSalesmanSolver\GeneticAlgorithm.exe" >> %SCRIPT%
+echo oLink.WorkingDirectory = "C:\TravellingSalesmanSolver" >> %SCRIPT%
 echo oLink.IconLocation = "C:\TravellingSalesmanSolver\icon.ico" >> %SCRIPT%
 echo oLink.Save >> %SCRIPT%
 
 cscript /nologo %SCRIPT%
 del %SCRIPT%
+GOTO END
+
+:CHECK_VERSION
+
+for /f "tokens=3" %%a in ('REG QUERY HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\VisualStudio\14.0\VC\Runtimes\x64 /v Bld ^|findstr /ri "REG_DWORD"') do set VCBLD=%%a
+if %VCBLD% LSS 0x7030 vcredist_x64.exe
+GOTO INSTALL
+
+:END
